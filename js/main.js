8516f0d0825512986e298472a48688dc8c824aa5
Marleen <aan_marleen@hotmail.com>
2018-05-03 20:43:47 +0200

    random kleuren

--- js/main.js
+++ js/main.js
@@ -4,28 +4,24 @@
 //let fishes = document.getElementsByTagName("fish")
 //let fish = fishes[0]
 
-let fishes = "";
-let bubbles = document.createElement("bubble")
-document.body.appendChild(bubbles)
-
+//let fishes = "";
 var i;
-for (i = 0; i < 100; i++) { 
+for (i = 0; i < 20; i++) { 
+    let screenSize = window.innerWidth
+    let randomNumber = Math.random() * screenSize * 0.9
+    
+    let screenSizeHeight = window.innerHeight
+    let randomNumber2 = Math.random() * screenSizeHeight * 0.8
+    
+    let randomNumber3 = Math.random() * 360
+    
     let fishes = document.createElement("fish")
     document.body.appendChild(fishes)
     
-    let screenSizeHeight = window.innerHeight*0.8
-    let randomNumber2 = Math.random() * screenSizeHeight
-
-    let screenSize = window.innerWidth*0.9
-    let randomNumber = Math.random() * screenSize
-
-    let screenSizeBubble = window.innerWidth*0.9
-    let randomNumberBubble = Math.random() * screenSize
-
     fishes.style.left = randomNumber + "px"
     fishes.style.top = randomNumber2 + "px"
-    fishes.style.webkitFilter = "hue-rotate(300deg)"
-    fishes.style.filter = "hue-rotate(300deg)"
+    fishes.style.webkitFilter = "hue-rotate(" + randomNumber3 + "deg)"
+    fishes.style.filter = "hue-rotate(" + randomNumber3 + "deg)"
 }
 
 //
@@ -34,9 +30,20 @@ for (i = 0; i < 100; i++) {
 //let bubbles = document.getElementsByTagName("bubble")
 //let bubble = bubbles[0]
 
-bubbles.style.left = randomNumberBubble + "px"
-bubbles.style.top = "0px"
+var b;
+for (b = 0; b < 20; b++) { 
+    let screenSizeBubble = window.innerWidth * 0.9
+    let randomNumberBubble = Math.random() * screenSizeBubble
+    
+    let screenSizeBubbleHeight = window.innerWidth * 0.9
+    let randomNumberBubbleHeight = Math.random() * screenSizeBubbleHeight
+    
+    let bubbles = document.createElement("bubble")
+    document.body.appendChild(bubbles)
 
+    bubbles.style.left = randomNumberBubble + "px"
+    bubbles.style.top = randomNumberBubbleHeight + "px"
+}
 //
 // roep een functie aan als alles geladen is
 //
@@ -44,6 +51,4 @@ bubbles.style.top = "0px"
 window.addEventListener("load", function () {
     console.log("start het aquarium")
 })
-
-
 
